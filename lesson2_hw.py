bc90f91157b71c4152ab558426ea3d4f72422c32
azamoviich <muhammadamin.nazirov@mail.ru>
2025-08-23 07:32:20 +0500

    lesson2_hw.py

---
+++ lesson2_hw.py
@@ -0,0 +1,45 @@
+###########
+#Homework 1 (Age Calculator)
+###########
+Name = input("Enter your name: ")
+Year_you_born = int(input("Enter the year you were born: "))
+Age = 2025 - Year_you_born
+print(f"Hello, {Name}, you are {Age} years old!")
+###########
+#Homework 2 (Car Name)
+###########
+txt = 'LMaasleitbtui'
+car1 = txt[::2]
+car2 = txt[1::2]
+print(car1, car2)
+###########
+#Homework 3 (Car Name)
+###########
+txt = 'MsaatmiazD'
+car1 = txt[::2]
+car2 = txt[::-1][::2]
+print(car1, car2)
+###########
+#Homework 4 (Extract Residence Area)
+###########
+txt = "I'am John. I am from London"
+word = txt.split()
+Area = Area [-1]
+print ("Residence Area :", Area)
+###########
+#Homework 4 (Reverse String)
+###########
+Text = input("Please input string")
+Result = Text[::-1]
+print("Results:", Result)
+###########
+#Homework 5 (Vowel Count)
+###########
+text = input("Enter a string: ")
+vowels = "aeiou"
+count = 0
+for X in text:
+    if X in vowels:
+        count += 1
+print("Number of vowels:", count)
+
