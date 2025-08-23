53201cb23d470cc257eab16ac8241b28b455881b
azamoviich <muhammadamin.nazirov@mail.ru>
2025-08-23 06:50:02 +0500

    Create lesson1_hw.py

---
+++ lesson1_hw.py
@@ -0,0 +1,29 @@
+##################
+# Homework 1 #####
+##################
+side = float(input("Enter side of square: "))
+print("Perimeter =", 4 * side)
+print("Area =", side ** 2)
+##################
+# Homework 2 #####
+##################
+diameter = float(input("Enter the diameter of the circle: "))
+pi = 3.14159
+length = pi * diameter
+print(f"Length (circumference) of circle = {length}")
+##################
+# Homework 3 #####
+##################
+A = float(input("Enter the length of side A: "))
+B = float(input("Enter the length of side B: "))
+mean = (A + B) / 2
+print("Mean =", mean)
+##################
+# Homework 4 ##### 
+##################
+A=float(input("For the first number, enter A: "))
+B=float(input("For the second number, enter B: "))
+print("Sum =", A + B)
+print("Product =", A * B)
+print("Square of A =", A ** 2)
+print("Square of B =", B ** 2)
